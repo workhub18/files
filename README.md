79fd424914f6445108be3359d1564d535f9da102
BitSails <mr.hthomp@gmail.com>
2017-03-23 22:31:25 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+#Simple stuff
+
+Just a simple program.
+
+
+# How to Compile
+
+Compile as a basic c++ program with
+
+~~~.bash
+
+g++ -std=c++14 files.cpp -o files
+
+~~~
---
+++ files.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main( )
+{
+   //create some data
+   short int a = -6730;
+   float b = 68.123; 
+   char c = 'J';
+ 
+   cout<<a" "<<b<<" "<<c;//display data to console (i.e. as formatted chars)
+
+   cout<<endl<<endl;
+ 
+   return 0;
+}
