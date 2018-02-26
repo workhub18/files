0400a6754eb545190014d179e00876fdb3b7445a
workhub <workhub@users.noreply.github.com>
2018-02-26 11:29:24 -0400

    Added binary file

---
+++ binary.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main( )
+{
+   //create some data
+   short int a = -6730;
+   float b = 68.123; 
+   char c = 'J';
+   
+   ofstream myfile{"abc.bin",ios::binary};
+   if(myfile)
+   {
+	   myfile.write(reinterpret_cast<char*>(&a),sizeof(a));
+	   myfile.write(reinterpret_cast<char*>(&b),sizeof(b));
+	   myfile.write(reinterpret_cast<char*>(&c),sizeof(c));
+   }
+   else
+   cout<<"error opening abc.txt";
+   myfile.close();
+   
+   cout<<a<<" "<<b<<" "<<c;//display data to console (i.e. as formatted chars)
+
+   cout<<endl<<endl;
+ 
+   return 0;
+  
+}
