6ec00b04fbb7c404bca32bb502e09e1164663af6
workhub <workhub@users.noreply.github.com>
2018-02-26 11:04:24 -0400

    Added text file

--- text.cpp
+++ text.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 using namespace std;
 
 int main( )
@@ -7,7 +8,16 @@ int main( )
    short int a = -6730;
    float b = 68.123; 
    char c = 'J';
- 
+   
+   ofstream myfile{abc.txt};
+   if(myfile)
+   {
+	   myfile<<a<<""<<b<<""<<c;
+   }
+   else
+   cout<<"error opening abc.txt";
+   myfile.close();
+   
    cout<<a<<" "<<b<<" "<<c;//display data to console (i.e. as formatted chars)
 
    cout<<endl<<endl;
